c5698fa5b46b31428ae4490c3927dda77a5ba37c
Oleg Salionov <oleg@salionov.ru>
2016-03-14 23:17:38 +1000

    mnr

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,23 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      number: props.store.getState().number
+    };
   }
 
 
   doIncrement() {
     this.props.store.dispatch({type: "INC_ACTION"});
-    console.log(this.props.store.getState());
     this.setState({
       number: this.props.store.getState().number
     })
   }
 
   render() {
-    var state = this.props.store.getState();
     return (
       <div>
-        <h1>Hello, world. {state.number}</h1>
+        <h1>Hello, world. {this.state.number}</h1>
         <button onClick={this.doIncrement.bind(this)}>Click</button>
 
       </div>
